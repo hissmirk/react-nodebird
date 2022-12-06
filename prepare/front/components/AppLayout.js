0a2ab32a0acf98648f2e0e5902b608b4e0eb3b23
현주엽/소프트브리지/화상커뮤니케이션플랫폼 <false9@kakao.com>
2022-12-06 10:58:31 +0900

    반응형 그리드

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,27 +1,37 @@
+import { useState } from "react";
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Menu, Input, Row, Col } from "antd";
+import styled from 'styled-components';
+
+import UserProfile from '../components/UserProfile';
+import LoginForm from '../components/LoginForm';
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
 
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <>
       <Menu mode="horizontal">
-        <Menu.Item>
+        <Menu.Item key="home">
           <Link href="/"><a>노드버드</a></Link>
         </Menu.Item>
-        <Menu.Item>
+        <Menu.Item key="profile">
           <Link href="/profile"><a>프로필</a></Link>
         </Menu.Item>
-        <Menu.Item>
-          <Input.Search enterButton style={{ verticalAlign: 'middle' }}/>
+        <Menu.Item key="mail">
+          <SearchInput enterButton />
         </Menu.Item>
-        <Menu.Item>
+        <Menu.Item key="signUp">
           <Link href="/signup"><a>회원가입</a></Link>
         </Menu.Item>
       </Menu>
       <Row gutter={8}>
         <Col xs={24} md={6}>
-          왼쪽 메뉴
+          {isLoggedIn ? <UserProfile setIsLoggedIn={setIsLoggedIn} /> : <LoginForm setIsLoggedIn={setIsLoggedIn} />}
         </Col>
         <Col xs={24} md={12}>
           {children}
