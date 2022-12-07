85f12377ecfe9d1b56ac6cf5d4361cb314eaafd0
현주엽/소프트브리지/화상커뮤니케이션플랫폼 <false9@kakao.com>
2022-12-07 18:17:10 +0900

    이미지 캐루셀 구현 (react-slick)

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import { useCallback, useState } from "react";
 import { PlusOutlined } from "@ant-design/icons";
+import ImagesZoom from "./ImagesZoom";
 
 const PostImages = ({ images }) => {
   const [showImagesZoom, setShowImagesZoom] = useState(false);
@@ -8,10 +9,15 @@ const PostImages = ({ images }) => {
   const onZoom = useCallback(() => {
     setShowImagesZoom(true);
   }, []);
+  const onClose = useCallback(() => {
+    setShowImagesZoom(false);
+  }, []);
+
   if (images.length === 1) {
     return (
       <>
         <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
       </>
     );
   }
@@ -21,6 +27,7 @@ const PostImages = ({ images }) => {
         <div>
           <img role="presentation" width="50%" src={images[0].src} alt={images[0].src} onClick={onZoom} />
           <img role="presentation" width="50%" src={images[1].src} alt={images[1].src} onClick={onZoom} />
+          {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
         </div>
       </>
     );
@@ -40,6 +47,7 @@ const PostImages = ({ images }) => {
           개의 사진 더보기
         </div>
       </div>
+      {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
     </>
   );
 };
