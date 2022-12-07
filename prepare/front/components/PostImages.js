d31c2e4829884cf66581f8b44aad4b8b9719e1a5
현주엽/소프트브리지/화상커뮤니케이션플랫폼 <false9@kakao.com>
2022-12-07 17:37:22 +0900

    이미지 구현

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -1,8 +1,46 @@
 import PropTypes from "prop-types";
+import { useCallback, useState } from "react";
+import { PlusOutlined } from "@ant-design/icons";
 
 const PostImages = ({ images }) => {
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+  if (images.length === 1) {
+    return (
+      <>
+        <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+      </>
+    );
+  }
+  if (images.length === 2) {
+    return (
+      <>
+        <div>
+          <img role="presentation" width="50%" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+          <img role="presentation" width="50%" src={images[1].src} alt={images[1].src} onClick={onZoom} />
+        </div>
+      </>
+    );
+  }
   return (
-    <div>PostImages</div>
+    <>
+      <div>
+        <img role="presentation" width="50%" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+        <div
+          role="presentation"
+          style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}
+          onClick={onZoom}
+        >
+          <PlusOutlined />
+          <br />
+          {images.length - 1}
+          개의 사진 더보기
+        </div>
+      </div>
+    </>
   );
 };
 
