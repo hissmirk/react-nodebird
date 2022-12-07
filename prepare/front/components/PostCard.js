baaeb5c14302e4a17414c67f6d79d89ea7ecbd14
현주엽/소프트브리지/화상커뮤니케이션플랫폼 <false9@kakao.com>
2022-12-07 15:19:06 +0900

    댓글 구현

--- prepare/front/components/PostCard.js
+++ prepare/front/components/PostCard.js
@@ -1,21 +1,22 @@
 import { useSelector } from "react-redux";
-import { Card, Popover, Button, Avatar } from "antd";
+import { Card, Popover, Button, Avatar, List, Comment } from "antd";
 import { EllipsisOutlined, HeartOutlined, MessageOutlined, HeartTwoTone, RetweetOutlined } from "@ant-design/icons";
 // import ButtonGroup from "antd/es/button/button-group";
 import PropTypes from "prop-types";
 
 import PostImages from "./PostImages";
 import { useCallback, useState } from "react";
+import CommentForm from "./CommentForm";
 
 const PostCard = ({ post }) => {
   const [liked, setLiked] = useState(false);
   const [commentFormOpened, setCommentFormOpened] = useState(false);
   const onToggleLike = useCallback(() => {
     setLiked((prev) => !prev);
-  },[]);
+  }, []);
   const onToggleComment = useCallback(() => {
     setCommentFormOpened((prev) => !prev);
-  },[]);
+  }, []);
   const id = useSelector((state) => state.user.me?.id);
   // const id = me && me?.id;
 
@@ -53,10 +54,22 @@ const PostCard = ({ post }) => {
       </Card>
       {commentFormOpened && (
         <div>
-          댓글 부분
+          <CommentForm post={post} />
+          <List
+            header={`${post.Comments.length}개의 댓글`}
+            itemLayout="horizontal"
+            dataSource={post.Comments}
+            renderItem={(item) => (
+              <li>
+                <Comment
+                  author={item.User.nickname}
+                  avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+                  content={item.content}
+                />
+              </li>
+            )}
+          />
         </div>)}
-      {/*<CommentForm />*/}
-      {/*<Comments />*/}
     </div>
   );
 };
