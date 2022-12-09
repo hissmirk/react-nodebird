f62cc73be4828fdaa7c31f14cc7dbb35386e29ac
현주엽/소프트브리지/화상커뮤니케이션플랫폼 <false9@kakao.com>
2022-12-09 15:51:19 +0900

    redux-saga 이펙트

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -1,13 +1,97 @@
-export default function* rootSaga() {
-  console.log(1);
-  yield;
-  console.log(2);
-  yield;
-  console.log(3);
-  yield 4;
+import { all, fork, call, put, take } from 'redux-saga/effects';
+
+function logInAPI() {
+  return axios.post('/api/login');
 }
 
-const generator = rootSaga();
-generator.next(); // 1
-generator.next(); // 2
-generator.next(); // 3...
+function* logIn() {
+  try {
+    const result = yield call(logInAPI); // 동기 호출
+    yield put({
+      type: 'LOG_IN_SUCCESS',
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: 'LOG_IN_FAILURE',
+      data: err.response.data,
+    });
+  }
+}
+
+function logOutAPI() {
+  return axios.post('/api/logout');
+}
+
+function* logOut() {
+  try {
+    const result = yield call(logOutAPI); // 동기 호출
+    yield put({
+      type: 'LOG_OUT_SUCCESS',
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: 'LOG_OUT_FAILURE',
+      data: err.response.data,
+    });
+  }
+}
+
+function logInAPI() {
+  return axios.post('/api/login');
+}
+
+function* logIn() {
+  try {
+    const result = yield call(logInAPI); // 동기 호출
+    yield put({
+      type: 'LOG_IN_SUCCESS',
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: 'LOG_IN_FAILURE',
+      data: err.response.data,
+    });
+  }
+}
+
+function addPostAPI() {
+  return axios.post('/api/post');
+}
+
+function* addPost() {
+  try {
+    const result = yield call(addPostAPI); // 동기 호출
+    yield put({
+      type: 'ADD_POST_SUCCESS',
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: 'ADD_POST_FAILURE',
+      data: err.response.data,
+    });
+  }
+}
+
+function* watchLogIn() {
+  yield take('LOG_IN_REQUEST', logIn);
+}
+
+function* watchLogOut() {
+  yield take('LOG_OUT_REQUEST', logOut);
+}
+
+function* watchAddPost() {
+  yield take('ADD_POST_REQUEST', addPost);
+}
+
+export default function* rootSaga() {
+  yield all([
+    fork(watchLogIn),
+    fork(watchLogOut),
+    fork(watchAddPost),
+  ]);
+}
