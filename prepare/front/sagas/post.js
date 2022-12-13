792e37c065c3d9d0c8df0badaeb2e824224b516f
현주엽/소프트브리지/화상커뮤니케이션플랫폼 <false9@kakao.com>
2022-12-13 19:34:56 +0900

    액션, 상태 정리

--- prepare/front/sagas/post.js
+++ prepare/front/sagas/post.js
@@ -1,31 +1,63 @@
 import { all, fork, put, takeLatest, delay } from "redux-saga/effects";
 import axios from "axios";
+import {
+  ADD_COMMENT_FAILURE,
+  ADD_COMMENT_REQUEST,
+  ADD_COMMENT_SUCCESS,
+  ADD_POST_FAILURE, ADD_POST_REQUEST,
+  ADD_POST_SUCCESS,
+  addComment
+} from "../reducers/post";
 
 function addPostAPI() {
   return axios.post('/api/post');
 }
 
-function* addPost() {
+function* addPost(action) {
   try {
-    // const result = yield call(addPostAPI); // 동기 호출
+    // const result = yield call(addPostAPI, action.data); // 동기 호출
     yield delay(1000);
     yield put({
-      type: 'ADD_POST_SUCCESS',
+      type: ADD_POST_SUCCESS,
     });
   } catch (err) {
     yield put({
-      type: 'ADD_POST_FAILURE',
+      type: ADD_POST_FAILURE,
+      data: err.response.data,
+    });
+  }
+}
+
+function addCommentAPI(data) {
+  return axios.post(`/api/post/${data.postId}/comment`, data);
+}
+
+function* addComment() {
+  try {
+    // const result = yield call(addCommentAPI); // 동기 호출
+    yield delay(1000);
+    yield put({
+      type: ADD_COMMENT_SUCCESS,
+    });
+  } catch (err) {
+    yield put({
+      type: ADD_COMMENT_FAILURE,
       data: err.response.data,
     });
   }
 }
 
 function* watchAddPost() {
-  yield takeLatest('ADD_POST_REQUEST', addPost);
+  yield takeLatest(ADD_POST_REQUEST, addPost);
+}
+
+function* watchAddComment() {
+  yield takeLatest(ADD_COMMENT_REQUEST, addComment);
 }
 
 export default function* postSaga() {
   yield all([
-    fork(watchAddPost()),
+    fork(watchAddPost),
+    fork(watchAddCooment),
   ]);
 };
--- prepare/front/sagas/user.js
+++ prepare/front/sagas/user.js
@@ -1,5 +1,17 @@
 import { all, fork, put, takeLatest, delay } from "redux-saga/effects";
 import axios from "axios";
+import {
+  LOG_IN_REQUEST,
+  LOG_IN_SUCCESS,
+  LOG_IN_FAILURE,
+  LOG_OUT_REQUEST,
+  LOG_OUT_SUCCESS,
+  LOG_OUT_FAILURE,
+  SIGN_UP_REQUEST,
+  SIGN_UP_SUCCESS,
+  SIGN_UP_FAILURE
+} from "../reducers/user";
+import signup from "../pages/signup";
 
 function logInAPI() {
   return axios.post('/api/login');
@@ -10,13 +22,13 @@ function* logIn(action) {
     // const result = yield call(logInAPI); // 동기 호출
     yield delay(1000);
     yield put({
-      type: 'LOG_IN_SUCCESS',
+      type: LOG_IN_SUCCESS,
       data: action.data,
     });
   } catch (err) {
     yield put({
-      type: 'LOG_IN_FAILURE',
-      data: err.response.data,
+      type: LOG_IN_FAILURE,
+      error: err.response.data,
     });
   }
 }
@@ -30,27 +42,51 @@ function* logOut() {
     // const result = yield call(logOutAPI); // 동기 호출
     yield delay(1000);
     yield put({
-      type: 'LOG_OUT_SUCCESS',
+      type: LOG_OUT_SUCCESS,
     });
   } catch (err) {
     yield put({
-      type: 'LOG_OUT_FAILURE',
-      data: err.response.data,
+      type: LOG_OUT_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+function signUpAPI() {
+  return axios.post('/api/signUp');
+}
+
+function* signUp() {
+  try {
+    // const result = yield call(signUpAPI); // 동기 호출
+    yield delay(1000);
+    yield put({
+      type: SIGN_UP_SUCCESS,
+    });
+  } catch (err) {
+    yield put({
+      type: SIGN_UP_FAILURE,
+      error: err.response.data,
     });
   }
 }
 
 function* watchLogIn() {
-  yield takeLatest('LOG_IN_REQUEST', logIn);
+  yield takeLatest(LOG_IN_REQUEST, logIn);
 }
 
 function* watchLogOut() {
-  yield takeLatest('LOG_OUT_REQUEST', logOut);
+  yield takeLatest(LOG_OUT_REQUEST, logOut);
+}
+
+function* watchSignUp() {
+  yield takeLatest(SIGN_UP_REQUEST, signup);
 }
 
 export default function* userSaga() {
   yield all([
     fork(watchLogIn),
     fork(watchLogOut),
+    fork(watchSignUp),
   ])
 }
